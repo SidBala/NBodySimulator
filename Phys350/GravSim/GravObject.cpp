f1f3ee1881da48b7a67d26e2161ec4a1d7b77a1d
sid1089@gmail.com <sidbala@users.noreply.github.com>
2010-02-16 04:39:33 +0000

    Fixed ObjectSim.cpp Iterator from const iterator to normal iterator

--- Phys350/GravSim/GravObject.cpp
+++ Phys350/GravSim/GravObject.cpp
@@ -27,33 +27,28 @@ CVec3::~CVec3(void)									//	Nothing to do here. No dynamic memory
 
 float CVec3::Norm()									//	Returns the 2-Norm of the vector
 {
-	//Nabil and Rudy: Insert Code Here
 	float norm;
 	norm = sqrt(x*x + y*y + z*z);
 	return norm;
 	
 }
 
-const CVec3 & CVec3::operator=(const CVec3 &v)						// Assignment Operator
+CVec3 & CVec3::operator=(const CVec3 &v)						// Assignment Operator
 {
-	//Nabil and Rudy: Insert Code Here
-	//Make sure to check for if object v is not this
-	if (this != v)
+	if (this != &v)
 	{
 		x = v.x;
 		y = v.y;
 		z = v.z;
-		return CVec3 (x,y,z);
 	}
-	else
-		return *this;
+	return *this;
 }
 
-// Nabil and Rudy: Implement the 6 friend operator functions here
+
 
 CVec3 operator+(const CVec3 &v1, const CVec3 &v2)
 {
-	// Nabil and Rudy: use this function as a reference and implement the 5 others too.
+	
 	float x,y,z;
 	x = v1.x + v2.x;
 	y = v1.y + v2.y;
@@ -97,14 +92,6 @@ CVec3 operator/(const CVec3 &v, const float f)
 	return CVec3 (x,y,z);
 }
 
-CVec3 operator/(const float f, const CVec3 &v)
-{
-	float x,y,z;
-	x = v.x / f;
-	y = v.y / f;
-	z = v.z / f;
-	return CVec3 (x,y,z);
-}
 
 CGravObject::CGravObject(void)
 {
--- Phys350/GravSim/ObjectSim.cpp
+++ Phys350/GravSim/ObjectSim.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "ObjectSim.h"
 
+class CVec3;
+
 
 CObjectSim::CObjectSim(void)
 {
@@ -13,12 +15,13 @@ CObjectSim::~CObjectSim(void)
 
 void CObjectSim::SimUpdate(DWORD milliseconds, std::vector<CGravObject> *ObjList)
 {
-	std::vector<CGravObject>::const_iterator i;
-	std::vector<CGravObject>::const_iterator j;
+	std::vector<CGravObject>::iterator i;
+	std::vector<CGravObject>::iterator j;
 
 	
 	CVec3 v3Dist;
 	CVec3 v3Acc;
+	v3Acc = v3Dist;
 	float fDist	= 0.0f;
 	float fDistCube = 0.0f;
 	float fForceMag = 0.0f; 
@@ -39,12 +42,12 @@ void CObjectSim::SimUpdate(DWORD milliseconds, std::vector<CGravObject> *ObjList
 				fDist = v3Dist.Norm();
 
 				fDistCube = fDist*fDist*fDist;
-				fRadiusCube = f
+				//fRadiusCube = f
 
-				if(fDist <= j->fRadius)
-					fForceMag = (j->fMass / fDistCube)
+			//	if(fDist <= j->fRadius)
+			//		fForceMag = (j->fMass / fDistCube)
 
-				fForceMag = (j->fMass / fDistCube)
+			//	fForceMag = (j->fMass / fDistCube)
 
 				v3Acc = v3Acc + ((j->fMass / fDistCube) * v3Dist);
 			}
